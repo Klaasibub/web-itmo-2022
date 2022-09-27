53371f2ff16c0cb1a5b1a173a4d968e738dea24d
Valery Zolkin <ya.justyell@yandex.ru>
2022-09-27 12:03:04 +0500

    Add JWT auth

---
+++ src/api/v1/auth.py
@@ -0,0 +1,108 @@
+from typing import Optional
+from datetime import datetime, timedelta
+
+from tortoise.exceptions import DoesNotExist
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from jose import jwt
+
+from models import User
+from api import verify_password, get_current_user
+from api.types import UserType, UserInType
+
+from settings import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+
+router = APIRouter(
+    tags=["auth"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+unauthorized_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Incorrect username or password",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=60)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@router.post("/token")
+async def get_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    try:
+        user = await UserType.from_queryset_single(User.get(username=form_data.username))
+    except DoesNotExist:
+        raise unauthorized_exception
+
+    if not verify_password(form_data.password, user.password):
+        raise unauthorized_exception
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    token = jsonable_encoder(access_token)
+    response = JSONResponse(content={
+        "access_token": access_token,
+        "token_type": "bearer",
+        "message": "You've successfully logged in. Welcome back!"
+    })
+    response.set_cookie(
+        "Authorization",
+        value=f"Bearer {token}",
+        httponly=True,
+        max_age=1800,
+        expires=1800,
+        samesite="Lax",
+        secure=True,
+    )
+
+    return response
+
+
+@router.post("/login")
+async def login(user_data: UserInType):
+    try:
+        user = await UserType.from_queryset_single(User.get(username=user_data.username))
+    except DoesNotExist:
+        raise unauthorized_exception
+
+    if not verify_password(user_data.password, user.password):
+        raise unauthorized_exception
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/me", response_model=UserType)
+async def get_user(user: UserType = Depends(get_current_user)):
+    return user
--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
+from api.v1 import auth
 from models import init_db
 from settings import APP_VERSION
 
@@ -16,31 +14,9 @@ async def startup_event():
     init_db(app)
 
 
-class Greeting(BaseModel):
-    message: str
-
-
-class User(BaseModel):
-    username: str
-
-
 @app.get("/health")
 async def healthcheck():
     return {'version': APP_VERSION}
 
 
-@app.get("/hello/{username}")
-async def hello_path(username: str):
-    return Greeting(message=f'Hello, {username}!')
-
-
-@app.get("/hello")
-async def hello_query(username: Union[str, None] = None):
-    if not username:
-        username = 'anonymous'
-    return Greeting(message=f'Hello, {username}!')
-
-
-@app.post("/hello")
-async def hello_body(user: User):
-    return Greeting(message=f'Hello, {user.username}!')
+app.include_router(auth.router)
