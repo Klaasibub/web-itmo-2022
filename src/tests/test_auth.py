7e8721df17dc9728ff3c0f8699489a2267ab10e2
Valeriy Zolkin <v.zolkin@3divi.ru>
2022-10-25 07:49:37 +0500

    Remove api tests

--- src/tests/test_auth.py
+++
@@ -1,4 +0,0 @@
-
-def test_me_without_token(test_app):
-    response = test_app.get("/me")
-    assert response.status_code == 401
--- src/tests/test_health.py
+++
@@ -1,5 +0,0 @@
-
-def test_ping(test_app):
-    response = test_app.get("/health")
-    assert response.status_code == 200
-    assert response.json().get("version")
